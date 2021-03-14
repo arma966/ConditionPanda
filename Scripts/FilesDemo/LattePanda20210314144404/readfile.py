98dd3f42ee175a668d601ff1aa5eecee7aef4e3f
arma96 <arma96@protonmail.com>
2021-03-14 22:01:11 +0100

    WIP data to json

--- Scripts/FilesDemo/LattePanda20210314144404/readfile.py
+++ Scripts/FilesDemo/LattePanda20210314144404/readfile.py
@@ -1,8 +1,11 @@
-import numpy as np
-import json
+from numpy import loadtxt
+from json import dump 
+from os import listdir
+from os.path import join, isfile
+from datetime import datetime, timedelta
 
 def getMetadata(FileName):
-    dictionary =  {}
+    metaDictionary =  {}
     read = True
     with open(FileName) as f:
         while read:
@@ -10,11 +13,11 @@ def getMetadata(FileName):
             if not(tmp == "\n"):
                 if not(tmp.find(":") == -1):
                     s = tmp.strip().split(": ",maxsplit = 1)
-                    dictionary[s[0]] = s[1]
+                    metaDictionary[s[0]] = s[1]
             else:
                 read = False
     f.close()
-    return dictionary
+    return metaDictionary
 
 def getData(FileName):
     # get the number of rows to skip
@@ -31,52 +34,136 @@ def getData(FileName):
     f.close()
     
     # Load data as numpy array
-    data= np.loadtxt(FileName, delimiter=",",skiprows=rowsToSkip)
+    data= loadtxt(FileName, delimiter=",",skiprows=rowsToSkip)
     return data
 
-readable = True
-FileName = "AI 1_MIN.txt"
-
-metaDataFile = getMetadata(FileName)
-
-
-data = getData(FileName)
-
-sensorList = {"AI 1": {
-              "Model": "J352SN",
-              "Machine": "MotorA",
-              "Place": "Rotor",
-              "Stat": {"MAX": {
-                  "dt": round((data[1,0]-data[0,0])*1000),
-                  "data": data[:,1].tolist(),
-                  "MU": "m/s2"
-                  },
-                "MIN": {
-                  "dt": round((data[1,0]-data[0,0])*1000),
-                  "data": data[:,1].tolist(),
-                  "MU": "m/s2"
+def buildDataDictionary(fileDir):
+    # Get the file list
+    fileList = [f for f in listdir(fileDir) if isfile(join(fileDir, f)) 
+                        and not(f.find('_') == -1)]
+    
+    # Build KPI dictionary
+    KPIdict = {}
+    for f in fileList:
+        try:
+            data = getData(f)
+            statName = f[f.find("_")+1:f.find(".")].replace(" ","_")
+            statDict = {"Dt": round((data[1,0]-data[0,0])*1000),
+                        "data": data[:,1].tolist()}
+            KPIdict[statName] = statDict
+        except:
+            print("Impossible to retrieve data from " + f)
+    
+    # Build sensor dictionary
+    sensorDict = {"AI 1": {
+                  "MOD": "J352SN",
+                  "MAC": "MotorA",
+                  "LOC": "Rotor",
+                  "KPI": KPIdict
                   }
+        }
+    
+    metaDataFile = getMetadata(fileList[0])
+    
+    # Build main dictionary
+    dt = datetime.strptime(metaDataFile["Start time"], '%m/%d/%Y %H:%M:%S.%f')
+    delta = timedelta(milliseconds=int(metaDataFile["Post time"]))
+    endTime = dt+delta
+    endTimeString = str(endTime.month) + '/' + str(endTime.day) + '/' + \
+                    str(endTime.year) + ' ' + str(endTime.hour) + ':' + \
+                    str(endTime.minute) + ':' + str(endTime.second) + '.' +\
+                    str(endTime.microsecond)[0:-3]
+    dataDict = {
+                "DV": "LattePanda",
+                "DAQ": "IOLITEd-1xACC",
+                "MU": "m/s2",
+                "S": sensorDict,
+                "AST": metaDataFile["Start time"],
+                "AET": endTimeString,
+                "SF": metaDataFile["Sample rate"],
+                "PT": metaDataFile["Post time"],
+        }
+    return dataDict
+
+def buildRawDictionary(fileDir):
+    # Get the file list
+    fileList = [f for f in listdir(fileDir) if isfile(join(fileDir, f)) 
+                        and f.find('_') == -1 and not(f.find('.txt') == -1) ]
+    
+    try:
+        data = getData(fileList[0])
+    except:
+        print("Impossible to retrieve raw data")
+    
+    
+    # Build sensor dictionary
+    sensorDict = {"AI 1": {
+                  "MOD": "J352SN",
+                  "MAC": "MotorA",
+                  "LOC": "Rotor",
+                  "Data": data[:,1].tolist()
                   }
-              }
-    }
+        }
+    
+    metaDataFile = getMetadata(fileList[0])
+    
+    # Build main dictionary
+    dt = datetime.strptime(metaDataFile["Start time"], '%m/%d/%Y %H:%M:%S.%f')
+    delta = timedelta(milliseconds=int(metaDataFile["Post time"]))
+    endTime = dt+delta
+    endTimeString = str(endTime.month) + '/' + str(endTime.day) + '/' + \
+                    str(endTime.year) + ' ' + str(endTime.hour) + ':' + \
+                    str(endTime.minute) + ':' + str(endTime.second) + '.' +\
+                    str(endTime.microsecond)[0:-3]
+    dataDict = {
+                "DV": "LattePanda",
+                "DAQ": "IOLITEd-1xACC",
+                "MU": "m/s2",
+                "S": sensorDict,
+                "AST": metaDataFile["Start time"],
+                "AET": endTimeString,
+                "SF": metaDataFile["Sample rate"],
+                "PT": metaDataFile["Post time"],
+        }
+    return dataDict
 
-dataDict = {
-            "DV": "LattePanda",
-            "DAQ": "IOLITEd-1xACC",
-            "S": sensorList,
-            "AST": metaDataFile["Start time"],
-            "AET": metaDataFile["Start time"],
-            "SF": metaDataFile["Sample rate"],
-            "PT": metaDataFile["Post time"],
-    }
+def to_json(data, name):
+    # Write the json file
+    with open(name +'.json', 'w') as f:
+        dump(data, f)
+    f.close()
 
+fileDir = "D:\\Documents\\Uni\\Tesi\\ConditionPanda\\Scripts\\FilesDemo\\LattePanda20210314144404"
 
-with open('prova.json', 'w') as f:
-    if readable:
-        json.dump(dataDict, f, indent = 4)
-    else:
-        json.dump(dataDict, f)
-f.close()
+dataDict = buildDataDictionary(fileDir)
+rawDict = buildRawDictionary(fileDir)
 
+dt = datetime.strptime(dataDict["AST"], '%m/%d/%Y %H:%M:%S.%f')
 
+y = str(dt.year)   
+if len(str(dt.month)) == 1:
+   m = '0' + str(dt.month)
+else:
+    m = str(dt.month)
+if len(str(dt.day)) == 1:
+   d = '0' + str(dt.day)
+else:
+    d = str(dt.day)
+if len(str(dt.hour)) == 1:
+   h = '0' + str(dt.hour)
+else:
+    h = str(dt.hour)
+if len(str(dt.minute)) == 1:
+   mi = '0' + str(dt.minute)
+else:
+    mi = str(dt.minute)
+if len(str(dt.second)) == 1:
+   s = '0' + str(dt.second)
+else:
+    s = str(dt.second)
+timestampName = y+m+d+h+mi+s
 
+dataDictName = timestampName + "Math"
+rawDictName = timestampName + "Raw"
+to_json(dataDict, dataDictName)
+to_json(rawDict, rawDictName)
