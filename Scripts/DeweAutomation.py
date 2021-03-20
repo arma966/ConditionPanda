06d01d6a995ac0383d539bb41991560249beb375
arma96 <arma96@protonmail.com>
2021-03-20 11:13:15 +0100

    Changing config file from json to ini (1)

--- Scripts/DeweAutomation.py
+++ Scripts/DeweAutomation.py
@@ -1,6 +1,7 @@
 import time
 import psutil
 from os.path import join
+from datetime import datetime
 
 def isRunning():
     isReady = False
@@ -41,6 +42,38 @@ def deweAuto(dw, FileName, DataDir):
         print("Exporting failed")
     dw.Measure()
     print("Measure completed")
+
+def DeweInit(dw, data_dir, setup_file):
+    print("Initializing Dewesoft... ")
     
+    dw.Init()
+    dw.Enabled = 1
+    dw.Visible = 1
+    print('done.')
+    
+    # set window dimensions
+    dw.Top = 0
+    dw.Left = 0
+    dw.Width = 1024
+    dw.Height = 768
+    
+    # build channel list
+    dw.Data.BuildChannelList()
+    
+    with open(setup_file, 'r') as file:
+        data = file.read()
+    file.close
+    
+    dw.LoadSetupFromXML(data)
+    
+    dw.SetMaindata_dir(data_dir)
+    
+def getMeasName():
+    MeasName = str(datetime.now())
+    MeasName = MeasName[:MeasName.find('.')]
+    MeasName = MeasName.replace(':','')
+    MeasName = MeasName.replace('-','')
+    MeasName = MeasName.replace(' ','')
+    return MeasName
     
     
